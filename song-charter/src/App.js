eab68ea2a737be76c8b7c0897e2c31e0ebfac011
Jordan Harrell <jwharrell0516@gmail.com>
2020-02-13 17:03:56 -0500

    JSON analysis

---
+++ song-charter/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Charter from './components/Charter.js'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Charter />
+      </div>
+    )
+  }
+}
